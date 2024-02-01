d659c632ffc200b46c7da3a045943ee45d9342cc
Thomas Vogel <t.vogel3110@gmail.com>
2024-02-01 15:00:32 +0100

    adhered to JSX's camelCase property naming

--- nextjs/components/Layout.js
+++ nextjs/components/Layout.js
@@ -9,8 +9,8 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>TSS-Captur</title>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
