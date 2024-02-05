51a83d3593fb8f72140f2a0764d5d64445f70678
Thomas Vogel <t.vogel3110@gmail.com>
2024-02-05 17:08:51 +0100

    added jobhash related status errors

--- nextjs/pages/api/[jobHash].js
+++ nextjs/pages/api/[jobHash].js
@@ -3,21 +3,28 @@ import path from 'path';
 
 export default async function handler(req, res) {
   const { jobHash } = req.query;
+  const paramsDir = path.join('./uploads', jobHash, 'params.json');
   const reportDir = path.join('./public/reports', jobHash);
-  const overviewDir = path.join(reportDir, 'interface', 'overview.html')
+  const overviewDir = path.join(reportDir, 'interface', 'overview.html');
   const errorDir = path.join(reportDir, 'error.log');
 
   try {
-    if (fs.existsSync(overviewDir)) {
-      // If the overview.html file exists, the job is considered completed
-      res.status(200).json({ status: 'completed' });
-    } else if (fs.existsSync(errorDir)) {
-      // If the error.log file exists, read its content and return it with the failed status
-      const err = fs.readFileSync(errorDir, 'utf8');
-      res.status(200).json({ status: 'failed', errorMessage: err });
+    if (fs.existsSync(paramsDir)) {
+      if (fs.existsSync(overviewDir)) {
+        // If the overview.html file exists, the job is considered completed
+        res.status(200).json({ status: 'completed' });
+      } else if (fs.existsSync(errorDir)) {
+        // If the error.log file exists, read its content and return it with the failed status
+        const err = fs.readFileSync(errorDir, 'utf8');
+        res.status(200).json({ status: 'failed', errorMessage: err });
+      } else {
+        // If none of the files exist, the job is still pending
+        res.status(200).json({ status: 'pending' });
+      }
     } else {
-      // If none of the files exist, the job is still pending
-      res.status(200).json({ status: 'pending' });
+      // If the job does not exist, return with the failed status
+      const err = 'Job does not exist';
+      res.status(200).json({ status: 'error', errorMessage: err });
     }
   } catch (error) {
     console.error(`Error checking status for job ${jobHash}:`, error);
--- nextjs/pages/status.js
+++ nextjs/pages/status.js
@@ -12,39 +12,43 @@ const statusPage = () => {
   // State to contain an error message
   const [errorMessage, setErrorMessage] = useState('');
 
-
   useEffect(() => {
-    const checkReportStatus = async () => {
-      // Ensures jobHash is defined before making the API call
-      if (!jobHash || (status !== 'pending' && status !== 'error')) {
-        return;
-      }
-
-      try {
-        // Makes an API call to check the job status using the jobHash 
-        setStatus('fetching');
-        const response = await axios.get(`/api/${jobHash}`);
-        const data = response.data;
-
-        // Handles fetched report status
-        if (data.status === 'completed') {
-          window.location.href = `/reports/${jobHash}/interface/overview.html`;
-        } else if (data.status === 'failed') {
-          setStatus('failed');
-          setErrorMessage(data.errorMessage);
-        } else {
-          setStatus('pending');
+    let intervalId;
+    if (status === 'pending') {
+      const checkReportStatus = async () => {
+        // Ensures jobHash is defined before making the API call
+        if (!jobHash) {
+          setStatus('error'); 
+          setErrorMessage('Job hash is missing.');
+          return;
         }
-      } catch (error) {
-        console.error('Error checking report status:', error);
-        setStatus('error'); 
-        setErrorMessage('An error occurred while fetching the report status.')
-      }
-    };
-  
-    const intervalId = setInterval(() => {
-      checkReportStatus();
-    }, 5000); // every 5 seconds
+
+        try {
+          // Makes an API call to check the job status using the jobHash 
+          setStatus('fetching');
+          const response = await axios.get(`/api/${jobHash}`);
+          const data = response.data;
+
+          // Handles fetched report status
+          if (data.status === 'completed') {
+            window.location.href = `/reports/${jobHash}/interface/overview.html`;
+          } else if (data.status === 'failed') {
+            setStatus('failed');
+            setErrorMessage(data.errorMessage);
+          } else {
+            setStatus('pending');
+          }
+        } catch (error) {
+          console.error('Error checking report status:', error);
+          setStatus('error'); 
+          setErrorMessage('An error occurred while fetching the report status.')
+        }
+      };
+    
+      intervalId = setInterval(() => {
+        checkReportStatus();
+      }, 5000); // every 5 seconds
+    }
 
     return () => clearInterval(intervalId);
   }, [jobHash, status]);
