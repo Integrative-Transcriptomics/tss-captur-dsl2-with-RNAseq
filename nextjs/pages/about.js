bb3fac8ea70c23de945895ac86fbef59c55d596f
Thomas Vogel <t.vogel3110@gmail.com>
2024-02-23 19:43:55 +0100

    added content for example/about pages

--- nextjs/pages/about.js
+++ nextjs/pages/about.js
@@ -5,7 +5,25 @@ export default function About() {
     <Layout>
       <div>
         <h1 className="h3 mb-3 text-gray-800">About Page</h1>
-        <p>This page provides information about the TSS-Captur project.</p>
+        <div>
+          Developed by Mathias Witte Paz
+          <br />Webified by Thomas Vogel
+        </div>
+        <p>
+          <br />To enable the characterization of transcripts in an explorative manner, we have developed TSS-Captur. TSS-Captur characterizes transcripts starting on clear TSS signals obtained from TSSpredator (Herbig, 2015) that cannot be associated to any known gene. To characterize the function of the transcript, TSS-Captur integrates tools using different approaches. The following figure describes the different steps and tools run by TSS-Captur.
+          <img src="https://user-images.githubusercontent.com/29492782/119950451-2403d880-bf9b-11eb-9b3e-326408f47c53.png" style={{ width: '75%', display: 'block', margin: 'auto' }}></img>
+          <br />It combines the advantages of comparative genomics and ab initio methods to account for many characteristics in the prediction. Furthermore, it uses the approach of transcriptional-signal based tools to combine TSS signals with a possible terminator. Moreover, it enables the user to explore the thermodynamic characteristics of possible identified ncRNA transcripts and visualize the secondary structure. Lastly, it facilitates the analysis of motifs within the promoter regions before each TSS signal to identify possible binding sites or other regulatory elements. All these results are combined within an interactive interface. This interface does not only provide an overview of the predicted results, but offers the possibility of getting the detailed information for each transcript and hence being able to reconstruct the predictions done by TSS-Captur.
+        </p>
+        <p>
+          References:
+          <br /><a href="https://doi.org/10.1093/nar/gkz400">CNIT <i class="fas fa-external-link-alt"></i></a>
+          <br /><a href="https://doi.org/10.1186/1471-2105-2-8">QRNA <i class="fas fa-external-link-alt"></i></a>
+          <br /><a href="https://doi.org/10.1016/S0022-2836(05)80360-2">BLAST <i class="fas fa-external-link-alt"></i></a>
+          <br /><a href="https://doi.org/10.1186/s12859-019-2704-x">RhoTermPredict <i class="fas fa-external-link-alt"></i></a>
+          <br /><a href="https://doi.org/10.1186%2Fgb-2007-8-2-r22">TransTermHP <i class="fas fa-external-link-alt"></i></a>
+          <br /><a href="https://doi.org/10.1093%2Fnar%2Fgkn188">RNAfold <i class="fas fa-external-link-alt"></i></a>
+          <br /><a href="https://doi.org/10.1093%2Fnar%2Fgkv416">MEME <i class="fas fa-external-link-alt"></i></a>
+        </p>
       </div>
     </Layout>
   );
--- nextjs/pages/example.js
+++ nextjs/pages/example.js
@@ -5,7 +5,12 @@ export default function Example() {
     <Layout>
       <div>
         <h1 className="h3 mb-3 text-gray-800">Example Data Page</h1>
-        <p>This is an example page to showcase example data.</p>
+        <p>
+          Campylobacter jejuni, a Gram-negative bacterium, is known to be one of the leading causes of bacterial gastroenteritis in humans. Its adaptability to a wide range of hosts and conditions, as well as its comparatively small genome of about 1.6 Mb, make it a good choice for the study of prokaryotic transcriptional mechanisms.
+          The following dataset includes four C. jejuni strains: <a href="https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=192222">NCTC11168 <i class="fas fa-external-link-alt"></i></a>, <a href="https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=info&id=354242">81-176 <i class="fas fa-external-link-alt"></i></a>, <a href="https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=407148">81116 <i class="fas fa-external-link-alt"></i></a> and <a href="https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=195099">RM1221 <i class="fas fa-external-link-alt"></i></a>. These strains were subjected to a high resolution transcriptome analysis by <a href="https://doi.org/10.1371/journal.pgen.1003495">Dugar et al. (2013) <i class="fas fa-external-link-alt"></i></a>.
+          <br />The dataset is available for download here: <a href="/example/input/dataset.zip"><i class="fas fa-download"></i></a>
+          <br />The corresponding report from TSS-Captur is available here: <a href="/example/report/Interface/overview.html">[Click here]</a>
+        </p>
       </div>
     </Layout>
   );
