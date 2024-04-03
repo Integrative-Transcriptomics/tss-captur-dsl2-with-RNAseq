e4050c38559847e8b053d7b288fccd36a8539649
Mathias Witte Paz <mathiaswittepaz@gmail.com>
2024-04-03 18:06:12 +0200

    adapting title

--- nextjs/pages/example.js
+++ nextjs/pages/example.js
@@ -6,20 +6,24 @@ export default function Example() {
     <Layout>
       <div>
         <h1 className="h3 mb-3 text-gray-800">Example Data Page</h1>
+        <h2 className="h4 mb-3 text-gray-800">Bacteroides thetaiotaomicron - plasmid</h2>
         <p>
           Bacteroides thetaiotaomicron is a Gram-negative bacterium that is known for its ability to degrade complex polysaccharides in the human gut.
-          The following small dataset includes one B. thetaiotaomicron strain and is especially usefull for testing the interface.
+          The following small dataset includes one B. thetaiotaomicron strain, focusing only on the plasmid of the orgnaims.
+          This is especially usefull for testing the interface.
           The data was computed based on the study by <a href="https://www.nature.com/articles/s41467-020-17348-5">Ryan et al. (2020) <i class="fas fa-external-link-alt"></i></a>.
           <br />The dataset is available for download here: <a href="/example/bacteroidesplasmid/dataset.zip"><i className="fas fa-download"></i></a>
           <br />The corresponding report from TSS-Captur is available here: <a href="/example/bacteroidesplasmid/report/Interface/overview.html">[Click here]</a>
         </p>
+        <h2 className="h4 mb-3 text-gray-800">Streptomyces coelicolor</h2>
 
         <p>
-          Streptomyces coelicolor is a soil-dwelling bacterium that is known for its complex life cycle and its ability to produce a wide range of secondary metabolites. The following dataset includes one S. coelicolor strain<a href="https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=100226">A3(2) <i class="fas fa-external-link-alt"></i></a>.
+          Streptomyces coelicolor is a soil-dwelling bacterium that is known for its complex life cycle and its ability to produce a wide range of secondary metabolites. The following dataset includes one S. coelicolor strain <a href="https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=100226">A3(2) <i class="fas fa-external-link-alt"></i></a>.
           The data was originally from the study by  <a href="https://www.nature.com/articles/ncomms11605">Jeong et al. (2016) <i class="fas fa-external-link-alt"></i></a>.
           <br />The dataset is available for download here: <a href="/example/sceolicolor/dataset.zip"><i className="fas fa-download"></i></a>
           <br />The corresponding report from TSS-Captur is available here: <a href="/example/scoelicolor/report/Interface/overview.html">[Click here]</a>
         </p>
+        <h2 className="h4 mb-3 text-gray-800">Campylobacter jejuni</h2>
 
         <p>
           Campylobacter jejuni, a Gram-negative bacterium, is known to be one of the leading causes of bacterial gastroenteritis in humans. Its adaptability to a wide range of hosts and conditions, as well as its comparatively small genome of about 1.6 Mb, make it a good choice for the study of prokaryotic transcriptional mechanisms.
